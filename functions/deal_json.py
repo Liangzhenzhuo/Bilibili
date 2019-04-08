b182df94e5e441b88b7732a7a82bc174e338e494
Liangzhenzhuo <2692789921@qq.com>
2019-04-08 19:41:16 +0800

    增加条件判断，消除异常

--- functions/deal_json.py
+++ functions/deal_json.py
@@ -13,20 +13,23 @@ def dict_get(dict_, objkey):
     :param objkey: 目标key
     :return: 目标key对应的value
     """
-    for key, value in dict_.items():
-        if key == objkey:
-            return value
-        else:
-            # 如果value是dict类型，则进行迭代
-            if isinstance(value, dict):
-                ret = dict_get(value, objkey)
-                if ret is not None:
-                    return ret
-            # 如果value是list类型，则依次进行迭代
-            elif isinstance(value, list):
-                for i in range(len(value)):
-                    ret = dict_get(value[i], objkey)
+    if isinstance(dict_, dict):
+        for key, value in dict_.items():
+            if key == objkey:
+                return value
+            else:
+                # 如果value是dict类型，则进行迭代
+                if isinstance(value, dict):
+                    ret = dict_get(value, objkey)
                     if ret is not None:
                         return ret
-    # 如果找不到指定的key，返回None
-    return None
+                # 如果value是list类型，则依次进行迭代
+                elif isinstance(value, list):
+                    for i in range(len(value)):
+                        ret = dict_get(value[i], objkey)
+                        if ret is not None:
+                            return ret
+        # 如果找不到指定的key，返回None
+        return None
+    else:
+        return None
